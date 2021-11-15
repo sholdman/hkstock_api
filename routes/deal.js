69a9edc6a8eb420874f138ac2d4092726514c6a7
Lai Lee <lai.lee@yawin.yesasia.com>
2021-11-15 12:07:52 +0800

    first commit

---
+++ routes/deal.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const scrapSteam = require('./scraper');
+var router = express.Router();
+
+/* GET deal  */
+router.get('/', function (req, res, next) {
+    //   res.render('index', { title: 'Express' });
+    const start = async function () {
+        const result = await scrapSteam();
+        res.send(result);
+    }
+    start();
+})
+
+module.exports = router;
